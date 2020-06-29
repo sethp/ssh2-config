3c5ad50cf01dd87c742abb406a6e4436e56a5bbf
Seth Pellegrino <seth@codecopse.net>
2020-06-29 08:19:59 -0700

    Handle (some) quotes, multiple consecutive delimiters
    Wherein I learn something about lifetimes and ownership: when readconf.c sees a quote, it modifies the underlying string to elide it. When I switched to borrows in 3085471, I complicated the ability of parse_line to make modifications to the string.
    
    Wrote down some questions and tradeoffs I see for now, as well as an #[ignored] test that checks for the behavior demonstrated in some of the integration tests.

--- src/lib.rs
+++ src/lib.rs
@@ -40,12 +40,13 @@ pub enum SSHOption<'a> {
     Port(u16),
 
     // Most things, right now
-    Unknown(&'a str, &'a str),
+    Unknown(String, &'a str),
 }
 
 // TODO: line + file info?
 #[derive(Debug)]
 pub enum Error<'a> {
+    UnmatchedQuote,
     TrailingGarbage(&'a str),
 
     InvalidPort(std::num::ParseIntError),
@@ -57,6 +58,7 @@ use std::fmt;
 impl<'a> fmt::Display for Error<'a> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
+            Error::UnmatchedQuote => write!(f, "no matching `\"` found"),
             Error::TrailingGarbage(ref garbage) => write!(f, "garbage at end of line: {}", garbage),
 
             Error::InvalidPort(ref err) => write!(f, "invalid port: {}", err),
@@ -74,12 +76,12 @@ impl<'a> SSHOption<'a> {
     }
 }
 
-fn do_the_inner_thing<'a>(keyword: &'a str, opt: &'a str) -> Result<'a, SSHOption<'a>> {
+fn do_the_inner_thing<'a, 'b>(keyword: &'b str, opt: &'a str) -> Result<'a, SSHOption<'a>> {
     use SSHOption::*;
     match keyword {
         "user" => Ok(User(opt)),
         "port" => Ok(Port(opt.parse().map_err(Error::InvalidPort)?)), // TODO: getservbyname
-        _ => Ok(Unknown(keyword, opt)),
+        _ => Ok(Unknown(keyword.to_owned(), opt)),
     }
 }
 
@@ -87,32 +89,60 @@ fn do_the_inner_thing<'a>(keyword: &'a str, opt: &'a str) -> Result<'a, SSHOptio
 // but we can't reject any that it would accept.
 // https://github.com/openssh/openssh-portable/blob/14beca57ac92d62830c42444c26ba861812dc837/readconf.c#L892
 fn parse_line<'a>(s: &'a str) -> Result<'a, Option<SSHOption<'a>>> {
-    let s = s.trim_start_matches(WHITESPACE);
-    if s.starts_with("#") {
-        return Ok(None);
-    }
-    let s = s.trim_end_matches(EOL_WHITESPACE);
-
-    // This might allow more than one = delimiter
-    let parts = s.splitn(3, WHITESPACE).collect::<Vec<_>>();
+    let mut is_quoted = false;
+    // TODO: how to deal with an internal `"`, as in foo" bar" (should parse to -> `foo bar`)
+    // Seems to require either:
+    // 1. Taking in a String so we can mutate it to elide the inner " like readconf.c does
+    //    - Pro: It's unlikely that anyone else wants to hold on to a reference to the string we're parsing
+    //    - Con: More awkward to use, requires lots of `to_string()`
+    //    - ??: `SSHOption`s need to own their string argument(s), but
+    //    - ??: Maybe we have to ^ anyway when it comes time to set options in c-land?
+    // 2. Allocating a new String copy with the inner " elided
+    //    - Pro: No mutation? Closer to the way readconf.c does it?
+    //    - Con: don't think there's a way to do the allocation _only_ if there's an inner quote (else a &str works)...
+    //           maybe if we're part of a larger whole that can optionally own things and we can assign ownership to
+    //           (optional ownership is what, a vec? should options optionally own things? what's the cost of an empty vec?)
+    //    - ??: `SSHOption`s need to own their string argument(s), but
+    //    - ??: Maybe we have to ^ anyway when it comes time to set options in c-land?
+    let parts = s
+        .trim_start_matches(WHITESPACE)
+        .trim_end_matches(EOL_WHITESPACE)
+        // TODO: sure would be nice to recover the bounded `splitn` semantic
+        // This will allow more than one = delimiter
+        .split(|ch: char| -> bool {
+            if ch == QUOTE {
+                is_quoted = !is_quoted;
+                true
+            } else if is_quoted {
+                false
+            } else {
+                WHITESPACE.contains(&ch)
+            }
+        })
+        .filter(|s| !s.is_empty())
+        .collect::<Vec<_>>();
 
     match parts.as_slice() {
+        [..] if is_quoted => Err(Error::UnmatchedQuote),
         [] => Ok(None),
-        [keyword, value] => Ok(Some(do_the_inner_thing(keyword, value)?)),
+        [word, ..] if word.starts_with("#") => Ok(None),
+        [keyword, value] => Ok(Some(do_the_inner_thing(&keyword.to_lowercase(), value)?)),
         [.., garbage] => Err(Error::TrailingGarbage(garbage)),
     }
 }
 
 // https://github.com/openssh/openssh-portable/blob/e073106f370cdd2679e41f6f55a37b491f0e82fe/misc.c#L323-L325
-const WHITESPACE: &[char] = &[' ', '=', '\t', '\r', '\n'];
-// TODO
-// const QUOTE: &[char] = &['"'];
+const WHITESPACE: &[char] = &[
+    ' ', '\t', '\r', '\n', /* for convenience, treat as blank */ '=',
+];
+const QUOTE: char = '"';
 
 const EOL_WHITESPACE: &[char] = &[' ', '\t', '\r', '\n', '\x0c' /* form feed */];
 
 #[test]
 fn it_works() {
     use SSHOption::*;
+    assert_eq!(parse_line("\r\n").expect("parse failed"), None);
     assert_eq!(
         parse_line("=# some comment\r\n").expect("parse failed"),
         None
@@ -126,6 +156,18 @@ fn it_works() {
     );
     assert_eq!(
         parse_line("user seth\r\n")
+            .expect("parse failed")
+            .expect("expected value"),
+        User("seth")
+    );
+    assert_eq!(
+        parse_line("\"user\" seth")
+            .expect("parse failed")
+            .expect("expected value"),
+        User("seth")
+    );
+    assert_eq!(
+        parse_line("\"user\" \"seth\"")
             .expect("parse failed")
             .expect("expected value"),
         User("seth")
@@ -161,8 +203,50 @@ mod tests {
                 .expect("expected value"),
             SSHOption::Port(22)
         );
+        assert_eq!(
+            SSHOption::parse("port  22")
+                .expect("parse failed")
+                .expect("expected value"),
+            SSHOption::Port(22)
+        );
 
         SSHOption::parse("port 222222").expect_err("parse should have failed");
         SSHOption::parse("port zz").expect_err("parse should have failed");
+    }
+
+    #[test]
+    fn simple_quoted_tokens() {
+        assert_eq!(
+            SSHOption::parse("port \"22\"")
+                .expect("parse failed")
+                .expect("expected value"),
+            SSHOption::Port(22)
+        );
+        assert_eq!(
+            SSHOption::parse("user \"one two\"")
+                .expect("parse failed")
+                .expect("expected value"),
+            SSHOption::User("one two")
+        );
+        SSHOption::parse("user \"hi").expect_err("parse should have failed");
+    }
+
+    #[test]
+    #[ignore] // TODO handle internal "
+    fn inner_quoted_tokens() {
+        assert_eq!(
+            SSHOption::parse("port 2\"2\"")
+                .expect("parse failed")
+                .expect("expected value"),
+            SSHOption::Port(22)
+        );
+        assert_eq!(
+            SSHOption::parse("user one\" two\"")
+                .expect("parse failed")
+                .expect("expected value"),
+            SSHOption::User("one two")
+        );
+
+        SSHOption::parse("user one\" \"two").expect_err("parse should have failed");
     }
 }
--- tests/config.rs
+++ tests/config.rs
@@ -18,12 +18,14 @@ fn hello_world() {
 
 #[test]
 fn hello_world2() {
-    let child = Command::new("ssh")
+    let mut child = Command::new("ssh")
         .args(&["-T", "-F", "/dev/null", "-G", "example.com"])
         .stdout(Stdio::piped())
         .stderr(Stdio::inherit())
         .spawn()
         .expect("failed to execute process");
+
+    child.wait().expect("child wasn't running");
 
     let mut lines = io::BufReader::new(child.stdout.expect("no stdout")).lines();
 
@@ -37,12 +39,14 @@ fn hello_world3() {
     // this is weird, but ??
     fs::write(&cfg_file, r#"=user eve"#).expect("failed writing config");
 
-    let child = Command::new("ssh")
+    let mut child = Command::new("ssh")
         .args(&["-T", "-F", cfg_file.to_str().unwrap(), "-G", "example.com"])
         .stdout(Stdio::piped())
         .stderr(Stdio::inherit())
         .spawn()
         .expect("failed to execute process");
+
+    child.wait().expect("child wasn't running");
 
     let mut lines = io::BufReader::new(child.stdout.expect("no stdout")).lines();
 
@@ -56,12 +60,13 @@ fn hello_world4() {
     // this is weird, but ??
     fs::write(&cfg_file, r#"=# comment"#).expect("failed writing config");
 
-    let child = Command::new("ssh")
+    let mut child = Command::new("ssh")
         .args(&["-T", "-F", cfg_file.to_str().unwrap(), "-G", "example.com"])
         .stdout(Stdio::piped())
         .stderr(Stdio::inherit())
         .spawn()
         .expect("failed to execute process");
+    child.wait().expect("child wasn't running");
 
     let mut lines = io::BufReader::new(child.stdout.expect("no stdout")).lines();
 
@@ -75,14 +80,75 @@ fn hello_world5() {
     // this is weird, but ??
     fs::write(&cfg_file, r#"=# comment"#).expect("failed writing config");
 
-    let child = Command::new("ssh")
+    let mut child = Command::new("ssh")
         .args(&["-T", "-F", cfg_file.to_str().unwrap(), "-G", "example.com"])
         .stdout(Stdio::piped())
         .stderr(Stdio::inherit())
         .spawn()
         .expect("failed to execute process");
 
+    child.wait().expect("child wasn't running");
+
     let mut lines = io::BufReader::new(child.stdout.expect("no stdout")).lines();
 
     assert_eq!(lines.nth(1).unwrap().unwrap(), "hostname example.com")
+}
+
+#[test]
+fn quoted_token_config() {
+    let dir = TempDir::new("foobar").unwrap();
+    let cfg_file = dir.path().join("hello_world6_config");
+    // this is valid, but `hello" "world is not
+    fs::write(
+        &cfg_file,
+        r#"hostname hello" world"
+    port 2"3""#,
+    )
+    .expect("failed writing config");
+
+    let mut child = Command::new("ssh")
+        .args(&["-T", "-F", cfg_file.to_str().unwrap(), "-G", "example.com"])
+        .stdout(Stdio::piped())
+        .stderr(Stdio::inherit())
+        .spawn()
+        .expect("failed to execute process");
+
+    child.wait().expect("child wasn't running");
+
+    let mut lines = io::BufReader::new(child.stdout.expect("no stdout")).lines();
+
+    assert_eq!(lines.nth(1).unwrap().unwrap(), "hostname hello world");
+    assert_eq!(lines.next().unwrap().unwrap(), "port 23");
+}
+
+#[test]
+fn multi_line_quoted_token_fails() {
+    let dir = TempDir::new("foobar").unwrap();
+    let cfg_file = dir.path().join("multi_line_quoted_token_config");
+    fs::write(
+        &cfg_file,
+        r#"hostname "foo
+    bar""#,
+    )
+    .expect("failed writing config");
+
+    let mut child = Command::new("ssh")
+        .args(&["-T", "-F", cfg_file.to_str().unwrap(), "-G", "example.com"])
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
+        .expect("failed to execute process");
+
+    let exit = child.wait().expect("child wasn't running");
+    assert!(!exit.success());
+
+    let mut lines = io::BufReader::new(child.stdout.expect("bad stdout")).lines();
+    assert!(lines.next().is_none());
+    let mut err_lines = io::BufReader::new(child.stderr.expect("bad stderr")).lines();
+
+    assert!(&err_lines
+        .next()
+        .unwrap()
+        .unwrap()
+        .ends_with("line 1: Missing argument."));
 }
